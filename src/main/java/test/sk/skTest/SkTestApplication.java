7bdca12aabe1e717c3b79211584f6c7b928ddb1f
sandykhanna <39180403+sandykhanna@users.noreply.github.com>
2024-02-29 16:54:32 +0530

    Add files via upload

---
+++ src/main/java/test/sk/skTest/SkTestApplication.java
@@ -0,0 +1,15 @@
+package test.sk.skTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkTestApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Printed");
+		SpringApplication.run(SkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/sk/skTest/SkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.sk.skTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
